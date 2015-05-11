628b49c8f918f425f9cb4da3baad3246941addd0
root <root@ip-172-31-45-81.us-west-2.compute.internal>
2015-05-11 20:22:27 +0000

    first commit

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN apt-get update && \
     apt-get install -y python-software-properties software-properties-common
 RUN add-apt-repository -y ppa:gluster/glusterfs-3.5 && \
     apt-get update && \
-    apt-get install -y nginx php5-fpm php5-mysql php-apc supervisor glusterfs-client
+    apt-get install -y nginx php5-fpm php5-mysql php-apc supervisor glusterfs-client curl mysql-proxy pwgen unzip
 
 ENV GLUSTER_PEER **ChangeMe**
 
@@ -19,6 +19,11 @@ ENV HTTP_PORT 80
 ENV HTTP_DOCUMENTROOT ${GLUSTER_VOL_PATH}
 ENV DEBUG 0
 
+ENV WORDPRESS_DB_HOSTS **ChangeMe**
+ENV WORDPRESS_DB_USER root
+ENV WORDPRESS_DB_PASSWORD **ChangeMe**
+ENV WORDPRESS_DB_NAME wordpress
+
 RUN mkdir -p /var/log/supervisor ${GLUSTER_VOL_PATH}
 WORKDIR ${GLUSTER_VOL_PATH}
 
@@ -26,7 +31,14 @@ RUN mkdir -p /usr/local/bin
 ADD ./bin /usr/local/bin
 RUN chmod +x /usr/local/bin/*.sh
 ADD ./etc/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+ADD ./etc/mysql/mysql-proxy.cnf /etc/mysql/mysql-proxy.cnf
 #ADD ./etc/nginx/sites-enabled/wordpress /etc/nginx/sites-enabled/wordpress
+
+RUN perl -p -i -e "s/ENABLED=\"false\"/ENABLED=\"true\"/g" /etc/default/mysql-proxy
+RUN perl -p -i -e "s/OPTIONS=\"\"/OPTIONS=\"--defaults-file=\/etc\/mysql\/mysql-proxy.cnf\"/g" /etc/default/mysql-proxy
+RUN perl -p -i -e "s/WORDPRESS_DB_HOSTS/${WORDPRESS_DB_HOSTS}/g" /etc/mysql/mysql-proxy.cnf
+RUN perl -p -i -e "s/WORDPRESS_DB_USER/${WORDPRESS_DB_USER}/g" /etc/mysql/mysql-proxy.cnf
+RUN perl -p -i -e "s/WORDPRESS_DB_PASSWORD/${WORDPRESS_DB_PASSWORD}/g" /etc/mysql/mysql-proxy.cnf
 
 #RUN echo "daemon off;" >> /etc/nginx/nginx.conf
 #RUN rm -f /etc/nginx/sites-enabled/default
--- bin/run.sh
+++ bin/run.sh
@@ -8,6 +8,14 @@ if [ "${GLUSTER_PEER}" == "**ChangeMe**" ]; then
    echo "ERROR: You did not specify "GLUSTER_PEER" environment variable - Exiting..."
    exit 0
 fi
+if [ "${WORDPRESS_DB_HOSTS}" == "**ChangeMe**" ]; then
+   echo "ERROR: You did not specify "WORDPRESS_DB_HOSTS" environment variable - Exiting..."
+   exit 0
+fi
+if [ "${WORDPRESS_DB_PASSWORD}" == "**ChangeMe**" ]; then
+   echo "ERROR: You did not specify "WORDPRESS_DB_PASSWORD" environment variable - Exiting..."
+   exit 0
+fi
 
 ALIVE=0
 for PEER in `echo "${GLUSTER_PEER}" | sed "s/,/ /g"`; do
@@ -30,11 +38,47 @@ echo "=> Mounting GlusterFS volume ${GLUSTER_VOL} from GlusterFS node ${PEER} ..
 mount -t glusterfs ${PEER}:/${GLUSTER_VOL} ${GLUSTER_VOL_PATH}
 
 if [ ! -e ${HTTP_DOCUMENTROOT}/index.php ]; then
-   echo "=> Installing wordpress in ${HTTP_DOCUMENTROOT}..."
+   echo "=> Installing wordpress in ${HTTP_DOCUMENTROOT} - this may take a while..."
    curl -o /tmp/wordpress.tar.gz "https://wordpress.org/wordpress-${WORDPRESS_VERSION}.tar.gz"
-   tar -xzf /tmp/wordpress.tar.gz -C /tmp/
+   tar -zxvf /tmp/wordpress.tar.gz -C /tmp/
    mv /tmp/wordpress/* ${HTTP_DOCUMENTROOT}/
    chown -R www-data:www-data ${HTTP_DOCUMENTROOT}
+fi
+
+if [ ! -e ${HTTP_DOCUMENTROOT}/wp-config.php ]; then
+   sed -e "s/database_name_here/$WORDPRESS_DB_NAME/
+   s/username_here/$WORDPRESS_DB_USER/
+   s/password_here/$WORDPRESS_DB_PASSWORD/
+   /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+   /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+   /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+   /'NONCE_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+   /'AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+   /'SECURE_AUTH_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+   /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
+   /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" ${HTTP_DOCUMENTROOT}/wp-config-sample.php > ${HTTP_DOCUMENTROOT}/wp-config.php
+   chown www-data:www-data ${HTTP_DOCUMENTROOT}/wp-config.php
+   chmod 640 ${HTTP_DOCUMENTROOT}/wp-config.php
+
+  # Download nginx helper plugin
+  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
+  unzip -o nginx-helper.*.zip -d ${HTTP_DOCUMENTROOT}/wp-content/plugins
+  chown -R www-data:www-data ${HTTP_DOCUMENTROOT}/wp-content/plugins/nginx-helper
+
+  # Activate nginx plugin and set up pretty permalink structure once logged in
+  cat << ENDL >> ${HTTP_DOCUMENTROOT}/wp-config.php
+\$plugins = get_option( 'active_plugins' );
+if ( count( \$plugins ) === 0 ) {
+  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
+  \$wp_rewrite->set_permalink_structure( '/%postname%/' );
+  \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
+  foreach ( \$pluginsToActivate as \$plugin ) {
+    if ( !in_array( \$plugin, \$plugins ) ) {
+      activate_plugin( '${HTTP_DOCUMENTROOT}/wp-content/plugins/' . \$plugin );
+    }
+  }
+}
+ENDL
 fi
 
 /usr/bin/supervisord
